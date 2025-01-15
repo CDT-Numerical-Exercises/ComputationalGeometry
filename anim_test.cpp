01c9d87da577ad6cadde83a7cdd505156cf410cb
Joshua Barrass <josh.barrass.work@gmail.com>
2025-01-15 12:49:33 +0000

    More sophisticated plot saving and animation generation

--- anim_test.cpp
+++ anim_test.cpp
@@ -9,21 +9,46 @@
 
 #include "helpers.h"
 
+void save_plot(const std::filesystem::path fn, const std::string gpscript,
+               const gsl_matrix *data) {
+  Gnuplot gp;
+  gp << "set terminal pngcairo size 350,262 enhanced font 'Verdana,10'\n";
+  gp << "set output " << fn << "\n"; // be warned -- this may not be sanitised
+  gp << gpscript << "\n";
+  for (int row = 0; row < data->size1; ++row) {
+    for (int col = 0; col < data->size2 - 1; ++col) {
+      gp << gsl_matrix_get(data, row, col) << " ";
+    }
+    gp << gsl_matrix_get(data, row, data->size2 - 1);
+    gp << "\n";
+  }
+  gp << "e\n";
+}
+
 int main() {
   Gnuplot gp;
 
   std::filesystem::create_directory("output");
 
-  gp << "set terminal pngcairo size 350,262 enhanced font 'Verdana,10'\n";
-  gp << "set output 'output/frame001.png'\n";
-  gp << "set yrange[-1:1]\n";
-  gp << "plot '-' with circles\n";
-  gp << "0.0 0.0\ne\n";
-  gp << "set terminal pngcairo size 350,262 enhanced font 'Verdana,10'\n";
-  gp << "set output 'output/frame002.png'\n";
-  gp << "plot '-' with circles\n";
-  gp << "0.0 0.0\n";
-  gp << "0.0 1.0\n";
+  const double points[] =
+  { 0.0, 0.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    -1.0, 0.0,
+    0.0, -1.0 };
+
+  const gsl_matrix_const_view all_data = gsl_matrix_const_view_array(points, 5, 2);
+
+  for (int points = 1; points <= 5; ++points) {
+    const gsl_matrix_const_view slice_view = gsl_matrix_const_submatrix(&all_data.matrix, 0, 0, points, 2);
+    char buf[50];
+    snprintf(buf, sizeof(buf), "output/frame%03d.png", points);
+    std::cout << buf << std::endl;
+    save_plot(buf, "set yrange[-1.5:1.5]\nset xrange[-1.5:1.5]\nplot '-' with circles\n", &slice_view.matrix);
+  }
+
+  // frames can then be merged together into an animation using ffmpeg
+  // see: https://trac.ffmpeg.org/wiki/Slideshow
 
   return 0;
 }
