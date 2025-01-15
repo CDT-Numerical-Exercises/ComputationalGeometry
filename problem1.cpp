c4905405d8fbe9f6c9cbf23ba08d4171033655f3
Joshua Barrass <josh.barrass.work@gmail.com>
2025-01-15 23:53:55 +0000

    Add delta parameter to supertriangle
    delta ensures all points lie inside the supertriangle, rather than at
    least one point lying on the supertriangle

--- problem1.cpp
+++ problem1.cpp
@@ -9,23 +9,20 @@
 #include "helpers.h"
 #include "delaunay.h"
 
-const double data[4*2] = {
+const double data[5*2] = {
   0, 0,
   1, 3,
   2, 1,
-  5, 4
+  5, 4,
+  2, 6
 };
 
 int main() {
-  const gsl_matrix_const_view d_view = gsl_matrix_const_view_array(data, 4, 2);
-  double st[6];
-  gsl_matrix_view supertriangle_view = gsl_matrix_view_array(st, 3, 2);
-  make_supertriangle(&d_view.matrix, &supertriangle_view.matrix);
-  print_matrix(supertriangle_view);
-
+  const gsl_matrix_const_view d_view = gsl_matrix_const_view_array(data, 5, 2);
+  
   Gnuplot gp;
-  gp << "set yrange[-1:10]\nset xrange[-3:8]\n";
-  gp << "plot '-' with circles, '-' with lines\n";
+  // gp << "set yrange[-1:10]\nset xrange[-3:8]\n";
+  gp << "plot '-' with circles, '-' with lines, '-' with lines\n";
   for (int row = 0; row < d_view.matrix.size1; ++row) {
     for (int col = 0; col < d_view.matrix.size2 - 1; ++col) {
       gp << gsl_matrix_get(&d_view.matrix, row, col) << " ";
@@ -34,7 +31,27 @@ int main() {
     gp << "\n";
   }
   gp << "e\n";
+
+  // construct supertriangle
+
+  double st[6];
+  gsl_matrix_view supertriangle_view = gsl_matrix_view_array(st, 3, 2);
+  make_supertriangle(&d_view.matrix, &supertriangle_view.matrix, 0);
+  print_matrix(supertriangle_view);
   
+  for (int row = 0; row < supertriangle_view.matrix.size1; ++row) {
+    for (int col = 0; col < supertriangle_view.matrix.size2 - 1; ++col) {
+      gp << gsl_matrix_get(&supertriangle_view.matrix, row, col) << " ";
+    }
+    gp << gsl_matrix_get(&supertriangle_view.matrix, row, supertriangle_view.matrix.size2 - 1);
+    gp << "\n";
+  }
+  gp << gsl_matrix_get(&supertriangle_view.matrix, 0, 0) << " " << gsl_matrix_get(&supertriangle_view.matrix, 0, 1) << "\n";
+  gp << "e\n";
+
+  // show with default delta
+  make_supertriangle(&d_view.matrix, &supertriangle_view.matrix);
+  print_matrix(supertriangle_view);
   for (int row = 0; row < supertriangle_view.matrix.size1; ++row) {
     for (int col = 0; col < supertriangle_view.matrix.size2 - 1; ++col) {
       gp << gsl_matrix_get(&supertriangle_view.matrix, row, col) << " ";
