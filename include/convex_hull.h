e40c6eff09d6b95a5c7623e26aeddf86aea374b4
Joshua Barrass <josh.barrass.work@gmail.com>
2025-02-07 16:01:23 +0000

    Convex hull implementation

--- include/convex_hull.h
+++ include/convex_hull.h
@@ -1,10 +1,13 @@
 #ifndef CONVEX_HULL_H
 #define CONVEX_HULL_H 1
 
+#include <vector>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 
 double calculate_D(const gsl_vector *p, const gsl_vector *q, const gsl_vector *r);
 double calculate_D(const gsl_matrix *X, const size_t pi, const size_t qi, const size_t ri);
+
+std::vector<size_t> convex_hull(const gsl_matrix *verts);
 
 #endif
--- src/convex_hull.cpp
+++ src/convex_hull.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <algorithm>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 
@@ -25,3 +27,69 @@ double calculate_D(const gsl_matrix *X, const size_t pi, const size_t qi,
   return calculate_D(&p.vector, &q.vector, &r.vector);
 }
 
+std::vector<size_t> argsort_by_x(const gsl_matrix *verts) {
+  // create a vector of indices
+  std::vector<size_t> args;
+  for (size_t i = 0; i < verts->size1; ++i) {
+    args.push_back(i);
+  }
+
+  // sort the indices based on the x coordinate of the corresponding
+  // vertex
+  std::sort(args.begin(), args.end(), [verts](const size_t &a, const size_t &b){
+    return gsl_matrix_get(verts, a, 0) < gsl_matrix_get(verts, b, 0); });
+
+  return args;
+}
+
+// returns a vector of integers corresponding to the indices of the
+// vertices in the input matrix. These are in order to create a
+// clockwise convex hull.
+std::vector<size_t> convex_hull(const gsl_matrix *verts) {
+  std::vector<size_t> points = argsort_by_x(verts);
+  const size_t n_points = points.size();
+
+  // iterate starting from i = 2
+  std::vector<size_t> Lupper = { points[0], points[1] };
+  for (size_t i = 2; i < points.size(); ++i) {
+    Lupper.push_back(points[i]);
+    
+    // remove points until the last turn is a right turn, or we don't
+    // have enough points to define a right turn.
+    // right turn is when D < 0
+    size_t current_length = Lupper.size();
+    while (current_length > 2 && calculate_D(verts, Lupper[current_length - 3], Lupper[current_length - 2], Lupper[current_length - 1]) > 0) {
+      // remove the middle of the last 3 points
+      // i.e. the second to last point
+      --current_length;
+      auto it = std::prev(Lupper.end());
+      Lupper.erase(it);
+    }
+  }
+
+  // iterate backwards
+  std::vector<size_t> Llower = { points[n_points - 1], points[n_points - 2] };
+  for (size_t i = n_points - 2; i > 0; --i) {
+    Llower.push_back(points[i-1]);
+
+    size_t current_length = Llower.size();
+    while (current_length > 2 && calculate_D(verts, Llower[current_length - 3], Llower[current_length - 2], Llower[current_length - 1]) > 0) {
+      // remove the middle of the last 3 points
+      // i.e. the second to last point
+      --current_length;
+      auto it = std::prev(Llower.end());
+      Llower.erase(it);
+    }
+  }
+
+  // remove the first and last points
+  Llower.erase(Llower.begin());
+  Llower.erase(Llower.end());
+
+  // glue the vectors together
+  auto end = Lupper.end();
+  Lupper.reserve(Lupper.size() + Llower.size());
+  Lupper.insert(end, Llower.begin(), Llower.end());
+
+  return Lupper;
+}
